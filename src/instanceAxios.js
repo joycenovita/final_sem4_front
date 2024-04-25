0f8e8d038e3385dcec1f7be4299c77db626a5c27
joycenovita <2281030@unai.edu>
2024-04-25 12:51:49 +0700

    modified route

--- src/instanceAxios.js
+++ src/instanceAxios.js
@@ -1,32 +1,32 @@
 // axios.js
-import axios from "axios";
-import router from "./router/index"; // Import router Vue
+import axios from 'axios'
+import router from './router/index' // Import router Vue
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Sesuaikan dengan URL API Laravel Anda
+  baseURL: 'http://127.0.0.1:8000', // Sesuaikan dengan URL API Laravel Anda
   timeout: 10000, // Sesuaikan dengan kebutuhan Anda
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-});
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+  }
+})
 
 // Membuat interceptors untuk menambahkan token ke header permintaan
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token"); // Anda harus mengganti ini dengan cara Anda sendiri untuk mendapatkan token
+    const token = localStorage.getItem('token') // Anda harus mengganti ini dengan cara Anda sendiri untuk mendapatkan token
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`
     }
-    return config;
+    return config
   },
   (error) => {
     // Menangani kesalahan jika ada
-    console.error("Request error:", error);
+    console.error('Request error:', error)
     // Redirect ke halaman login jika terjadi kesalahan
-    router.push("/login");
-    return Promise.reject(error);
+    router.push('/sign-in')
+    return Promise.reject(error)
   }
-);
+)
 
-export default instance;
+export default instance
--- src/router/index.js
+++ src/router/index.js
@@ -16,91 +16,118 @@ import Challenge from '../views/Challenge.vue'
 import ChallengeAdd from '../views/ChallengeAdd.vue'
 import ChallengeDetail from '../views/ChallengeDetail.vue'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/sign-in',
       name: 'Sign In',
-      component: SignIn
+      component: SignIn,
+      meta: { requiresAuth: true }
     },
     {
       path: '/sign-up',
       name: 'Sign Up',
-      component: SignUp
+      component: SignUp,
+      meta: { requiresAuth: true }
     },
     {
       path: '/home',
       name: 'Home',
-      component: Home
+      component: Home,
+      meta: { requiresAuth: true }
     },
     {
       path: '/profile',
       name: 'Profile',
-      component: Profile
+      component: Profile,
+      meta: { requiresAuth: true }
     },
     {
       path: '/journal',
       name: 'Journal',
       component: Journal,
+      meta: { requiresAuth: true }
     },
     {
       path: '/journal/add',
       name: 'JournalAdd',
-      component: JournalAdd
+      component: JournalAdd,
+      meta: { requiresAuth: true }
     },
     {
       path: '/journal/:id',
       name: 'JournalDetail',
-      component: JournalDetail
+      component: JournalDetail,
+      meta: { requiresAuth: true }
     },
     {
       path: '/mood',
       name: 'Mood',
-      component: Mood
+      component: Mood,
+      meta: { requiresAuth: true }
     },
     {
       path: '/mood/add',
       name: 'MoodAdd',
-      component: MoodAdd
+      component: MoodAdd,
+      meta: { requiresAuth: true }
     },
     {
       path: '/mood/:id',
       name: 'MoodDetail',
-      component: MoodDetail
+      component: MoodDetail,
+      meta: { requiresAuth: true }
     },
     {
       path: '/resources',
       name: 'Resources',
-      component: Resources
+      component: Resources,
+      meta: { requiresAuth: true }
     },
     {
       path: '/resources/add',
       name: 'ResourcesAdd',
-      component: ResourcesAdd
+      component: ResourcesAdd,
+      meta: { requiresAuth: true }
     },
     {
       path: '/resources/:id',
       name: 'ResourcesDetail',
-      component: ResourcesDetail
+      component: ResourcesDetail,
+      meta: { requiresAuth: true }
     },
     {
       path: '/challenge',
       name: 'Challenge',
-      component: Challenge
+      component: Challenge,
+      meta: { requiresAuth: true }
     },
     {
       path: '/challenge/add',
       name: 'ChallengeAdd',
-      component: ChallengeAdd
+      component: ChallengeAdd,
+      meta: { requiresAuth: true }
     },
     {
       path: '/challenge/:id',
       name: 'ChallengeDetail',
-      component: ChallengeDetail
-    },
+      component: ChallengeDetail,
+      meta: { requiresAuth: true }
+    }
   ]
+})
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    const token = localStorage.getItem
+    if (!token) {
+      next('/sign-in')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
